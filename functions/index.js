c2cdf30f1cbf01417fc0e8acaaeae2b9da3e5345
zcaalock <zcaalock@yahoo.com>
2019-07-16 19:05:43 +0200

    firebase install

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+
+admin.initializeApp()
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.getUsers = functions.https.onRequest((req, res) => {
+  admin.firestore().collection('users').get()
+    .then(data => {
+      let users = [];
+      data.forEach(doc => {
+        users.push(doc.data());
+      })
+        return res.json(users)
+    })
+    .catch(err=>console.error(err))
+})
+
+exports.createUser = functions.https.onRequest((req, res) => {
+  const newUser = {
+    body: req.body.body,
+    userHandle: req.bofy.userHandle,
+    createAt: admin.firestore.Timestamp.fromDate(new Date())
+  }
+
+  admin.firestore()
+        .collection('users')
+        .add(newUser)
+        .then(doc => {
+          res.json({ message: `document ${doc.id} created successfuly`})
+        })
+        .catch(err => {
+          res.status(500).json({error: 'something went wrong'})
+          console.error(err)
+        })
+})
--- src/actions/users.js
+++ src/actions/users.js
@@ -2,9 +2,9 @@ import users from '../apis/server'
 import * as types from './types'
 
 export const fetchUsers = () => async dispatch => {
-  const responce = await users.get('/users')
+  const responce = await users.get('https://us-central1-quickstart-1561998550467.cloudfunctions.net/getUsers')
   //console.log(responce)
-  console.log(responce)
+  console.log(responce.data)
   dispatch({type: types.FETCH_USERS, payload: responce.data})
   
 }
