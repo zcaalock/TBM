1dfc8d69153d560bec8429134d2eb29b37e189d1
zcaalock <zcaalock@yahoo.com>
2019-07-16 17:56:46 +0200

    firebase_api api

--- src/actions/users.js
+++ src/actions/users.js
@@ -4,6 +4,7 @@ import * as types from './types'
 export const fetchUsers = () => async dispatch => {
   const responce = await users.get('/users')
   //console.log(responce)
-  dispatch({type: types.FETCH_USERS, payload: responce.data.users})
+  console.log(responce)
+  dispatch({type: types.FETCH_USERS, payload: responce.data})
   
 }
--- src/apis/server.js
+++ src/apis/server.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://quickstart-1561998550467.firebaseapp.com/api/'
+  //baseURL: 'https://quickstart-1561998550467.firebaseapp.com/api/'
   //baseURL: 'http://localhost:3001'
   //baseURL: 'https://tbm-api.herokuapp.com/'
+  baseURL: 'https://quickstart-1561998550467.firebaseio.com/'
 })
