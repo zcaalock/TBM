de029bc049c7cb17d944e9399d2d76225526df8b
zcaalock <zcaalock@yahoo.com>
2019-07-17 23:34:13 +0200

    login route

--- functions/index.js
+++ functions/index.js
@@ -280,6 +280,36 @@ app.post('/signup', (req, res) => {
     })
 })
 
+//login route 
+
+app.post('/login', (req,res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+  let errors = {}
+  
+  if (isEmpty(user.email)) errors.email = 'Must not be empty'  
+  if (isEmpty(user.password)) errors.password = 'Must not be empty'
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+  firebase
+  .auth()
+  .signInWithEmailAndPassword(user.email, user.password)
+  .then(data => {
+    return data.user.getIdToken()
+  })
+  .then((token) => {
+    return res.json({token})
+  })
+  .catch((err) => {
+    console.log(err)
+    if(err.code === 'auth/wrong-password'){
+      return res.status(403).json({general: 'Wrong credentials, please try again'})
+    } else {return res.status(500).json({error: err.code})}
+  })
+})
+
 
 exports.api = functions.region('europe-west2').https.onRequest(app)
 
