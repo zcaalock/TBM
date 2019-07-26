880cb357821c97721c21df6dc5732536d5f094c4
zcaalock <zcaalock@yahoo.com>
2019-07-26 21:39:50 +0200

    persistent state

--- src/components/App.js
+++ src/components/App.js
@@ -17,9 +17,10 @@ import Settings from './middle/Settings'
 import unAuth from './middle/unAuth'
 import jwtDecode from 'jwt-decode';
 
+import { Provider } from 'react-redux'
 import store from '../reducers/index'
-import {SET_AUTHENTICATED} from '../actions/types'
-import {logoutUser, getUserData} from '../actions/users'
+import { SET_AUTHENTICATED } from '../actions/types'
+import { logoutUser, getUserData } from '../actions/users'
 
 //axios.defaults.baseURL = 'https://europe-west2-quickstart-1561998550467.cloudfunctions.net/api'
 
@@ -39,22 +40,24 @@ if (token) {
 const App = () => {
   return (
     <div style={{ margin: '10px' }} className="container">
-      <Router history={history}>
-        <div>
-          <Route >
-            <Route exact path="/" component={LandingPage}/>
-            <Route exact path="/unAuth" component={unAuth} />
-            <Route exact path="/login" component={Login}/>
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/settings" component={(props)=>(<><LeftMenu {...props}/> <Settings {...props}/></>)} />            
-            <Route exact path="/filters" component={(props)=>(<><LeftMenu {...props}/> <div className='article'><Filters {...props}/></div></>)} />
-            <Route path='/filters/:selector/:item' component={(props)=>(<><LeftMenu {...props}/> <div className='article'><Filters {...props}/><FilterTable {...props}/></div></>)}/>
-            <Route path='/filters/:selector/:item/pulses/:id' component={Details}/>                      
-            <Route path="/boards/:id" component={(props)=>(<><LeftMenu {...props}/> <Boards {...props}/></>)} />            
-            <Route path="/boards/:id/pulses/:id" component={Details} />
-          </Route>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router history={history}>
+          <div>
+            <Route >
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/unAuth" component={unAuth} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/settings" component={(props) => (<><LeftMenu {...props} /> <Settings {...props} /></>)} />
+              <Route exact path="/filters" component={(props) => (<><LeftMenu {...props} /> <div className='article'><Filters {...props} /></div></>)} />
+              <Route path='/filters/:selector/:item' component={(props) => (<><LeftMenu {...props} /> <div className='article'><Filters {...props} /><FilterTable {...props} /></div></>)} />
+              <Route path='/filters/:selector/:item/pulses/:id' component={Details} />
+              <Route path="/boards/:id" component={(props) => (<><LeftMenu {...props} /> <Boards {...props} /></>)} />
+              <Route path="/boards/:id/pulses/:id" component={Details} />
+            </Route>
+          </div>
+        </Router>
+      </Provider>
     </div>
   )
 }
