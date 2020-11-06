aa66b396946c6020e6e30884d8d06d10be7abcbf
zcaalock <zcaalock@yahoo.com>
2020-11-06 20:49:27 +0100

    semantic css fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import './css/index.css'
 import './translations/i18n'
+import 'semantic-ui-css/semantic.min.css'
 
 import axios from 'axios'
 
