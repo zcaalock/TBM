d5d09b701b09b75c262d81fbb3ef0af2c622f7f8
zcaalock <zcaalock@yahoo.com>
2020-11-07 22:32:31 +0100

    css

--- src/components/middle/Charts/Head.js
+++ src/components/middle/Charts/Head.js
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next"
 function Head() {
   const { t } = useTranslation()
   return (
-    <div style={{width: '90%', backgroundColor: 'white', marginLeft: '-20px', paddingLeft: '20px'}}>
+    <div style={{width: '80%', backgroundColor: 'white', marginLeft: '-20px', paddingLeft: '20px'}}>
       <div className="head-vertical-segment" style={{ paddingBottom: '20px' }}>
         <h3>{t('Charts')}: </h3>
       </div>      
